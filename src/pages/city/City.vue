220d8c5c6c8a3ac5179b69b226f1ac99d9aa3298
Baihaihua <bhh6551@163.com>
2019-05-22 22:44:29 +0800

    city-search-container

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,19 @@
 <template>
+<div>
     <CityHeader />
+    <CitySearch />
+</div>
 </template>
 
 <script>
 import CityHeader from "./CityHeader";
+import CitySearch from "./CitySearch";
 
 export default {
     name: "City",
     components: {
         CityHeader,
+        CitySearch,
     }
 }
 </script>
---
+++ src/pages/city/CitySearch.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="search-cont">
+        <input class="input" type="text" placeholder="请输入城市或拼音"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CitySearch",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/variables.scss";
+@import "~@/assets/styles/mixins.scss";
+
+.search-cont {
+    height: 36px;
+    background-color: $bgColor;
+    text-align: center;
+}
+
+.input {
+    border: none;
+    outline: none;
+    border-radius: 4px;
+    width: 85%;
+    height: $inputHeight;
+    padding: 0 10px;
+    color: $darkTextColor;
+}
+
+</style>
