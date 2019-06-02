01da91181e716fdb23bf8e240eb56cd31e0fc92d
Baihaihua <bhh6551@163.com>
2019-06-02 20:00:41 +0800

    detail-header, animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,83 +1,25 @@
 <template>
     <div>
-        <div
-            class="banner"
-            @click="clickBanner"
-        >
-            <img class="banner-img" src="https://p3-q.mafengwo.net/s13/M00/0D/DD/wKgEaVyp7uGAXdeGAAyNcQ5FafQ55.jpeg" alt="">
-            <div class="banner-info">
-                <span class="banner-title">扬州的春天是花的天堂</span>
-                <span class="banner-number"><Icon className="#icon-zhaopiantubiao" />&nbsp;32</span>
-            </div>
-        </div>
-        <Gallery
-            v-show="showGallery"
-            :imgs="imgs"
-            @close="closeGallery"
-        />
+        <DetailBanner />
+        <DetailHeader />
+        <div class="content"></div>
     </div>
-    
 </template>
 
 <script>
-import Icon from "components/Icon";
-import Gallery from "components/Gallery";
+import DetailBanner from "./components/DetailBanner";
+import DetailHeader from "./components/DetailHeader";
 export default {
     name: "Detail",
     components: {
-        Icon,
-        Gallery,
+        DetailBanner,
+        DetailHeader,
     },
-    data () {
-        return {
-            showGallery: false,
-            imgs: [
-                "https://p3-q.mafengwo.net/s13/M00/0D/DD/wKgEaVyp7uGAXdeGAAyNcQ5FafQ55.jpeg",
-                "https://b4-q.mafengwo.net/s11/M00/95/AC/wKgBEFpcReGARFs1AAGO3jkiTN816.jpeg",
-            ],
-        }
-    },
-    methods: {
-        clickBanner() {
-            this.showGallery = true;
-        },
-        closeGallery() {
-            this.showGallery = false;
-        }
-    }
 }
 </script>
 
 <style lang="scss" scoped>
-.banner {
-    position: relative;
-    overflow: hidden;
-    height: 0;
-    padding-bottom: 55%;
-}
-.banner-img {
-    width: 100%;
-}
-.banner-info {
-    padding: 0 10px;
-    display: flex;
-    align-items: center;
-    height: 28px;
-    color: #fff;
-    background-image: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,.8));
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-}
-.banner-title {
-    flex: 1;
-    font-size: 16px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-}
-.banner-number {
-    width: 40px;
+.content {
+    height: 1000px;
 }
 </style>
---
+++ src/pages/detail/components/DetailHeader.vue
@@ -0,0 +1,91 @@
+<template>
+<div>
+    <div class="header-abs" v-show="showAbs">
+        <router-link to="/">
+            <div class="return iconfont">&#xe660;</div>
+        </router-link>
+    </div>
+    <div
+        class="header-fixed"
+        v-show="!showAbs"
+        :style="opacityStyle"
+    >
+        <span>
+            <router-link to="/">
+                <div class="return iconfont">&#xe660;</div>
+            </router-link>
+        </span>
+        <span class="header-title">推荐详情</span>
+    </div>
+</div>
+</template>
+
+<script>
+const h = 150;
+export default {
+    name: "DetailHeader",
+    data() {
+        return {
+            showAbs: true,
+            timer: null,
+            opacityStyle: {
+                opacity: 0,
+            },
+        }
+    },
+    methods: {
+        handleScroll() {
+            const scrollTop = document.documentElement.scrollTop;
+            this.showAbs = scrollTop < 5;
+            const opacity = scrollTop >= h ? 1 : (scrollTop / 150)
+            this.opacityStyle = {opacity}
+            this.timer = null;
+        }
+    },
+    activated() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~styles/mixins.scss";
+.return {
+    text-align: center;
+    font-size: 20px;
+}
+.header-abs {
+    height: 46px;
+    line-height: 46px;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    .return {
+        height: 36px;
+        width: 36px;
+        line-height: 36px;
+        border-radius: 50%;
+        margin-left: 10px;
+        margin-top: 10px;
+        background-color: rgba(0,0,0,.5);
+        color: #eee;
+    }
+}
+.header-fixed {
+    @include header;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    .return {
+        width: 46px;
+        color: #fff;
+    }
+    .header-title {
+        margin-right: 46px;
+        flex: 1;
+        text-align: center;
+    }
+}
+</style>
